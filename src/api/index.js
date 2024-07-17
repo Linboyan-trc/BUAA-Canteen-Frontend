afc791b22bd732ca03cc749334e8c5328db88471
zz-orb <22373474@buaa.edu.cn>
2024-07-18 00:24:18 +0800

    添加了一些api

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import http from "@/utils/http";
 
 const apiClient = axios.create({
     baseURL: process.env.VUE_APP_API_URL || 'http://localhost:8000/api',
@@ -15,4 +16,114 @@ export default {
         return apiClient.get(`/dishes/${id}`);
     }
     //TODO 其他API方法
+}
+
+// 登录
+export const login = ({email, password}) => {
+    return http({
+        url: '/login/',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: {
+            email,
+            password
+        }
+    })
+}
+
+
+// 注册
+export const Register = ({email, username, password}) => {
+    return http({
+        url: '/register/',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        data: {
+            username,
+            password,
+            email
+        }
+    })
+}
+
+// 评论帖子
+export const doComment = ({data}) => {
+    return http({
+        url: 'user/comment/',
+        method: 'POST',
+        data: data
+    })
+}
+
+// 用户收藏
+export const doCollect = ({id}) => {
+    return http({
+        url: 'user/collect/',
+        method: 'POST',
+        data: {id}
+    })
+}
+
+//用户取消收藏
+export const cancelCollect = ({id}) => {
+    return http({
+        url: 'user/uncollect/',
+        method: 'DELETE',
+        data: {id}
+    })
+}
+
+//用户吃过菜品
+export const doAte = ({id}) => {
+    return http({
+        url: 'user/ate/',
+        method: 'POST',
+        data: {id}
+    })
+}
+
+//用户取消吃过菜品
+export const cancelAte = ({id}) => {
+    return http({
+        url: 'user/no-ate/',
+        method: 'DELETE',
+        data: {id}
+    })
+}
+
+export const getComment = ({id, offset}) => {
+    return http({
+        url: '/comment/main/',
+        method: 'POST',
+        data: {
+            id,
+            offset
+        }
+    })
+}
+
+export const loadReplies = ({id, offset}) => {
+    return http({
+        url: '/comment/reply/',
+        method: 'POST',
+        data: {
+            id,
+            offset
+        }
+    })
+}
+
+// 帖子详情
+export const postDetail = ({id}) => {
+    return http({
+        url: '/post/detail/',
+        method: 'POST',
+        data: {
+            id
+        }
+    })
 }
