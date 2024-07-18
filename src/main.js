eb7d017fc7665bdca958b57038f35d28ce12adbd
cubeSugar <wxbcly@163.com>
2024-07-18 19:24:50 +0800

    修改css文件路径，添加了一个css文件

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+import './assets/css/main.css'
 
 import { createApp} from 'vue'
 import App from './App.vue'
@@ -6,7 +6,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api';
+axios.defaults.baseURL = import.meta.env.VUE_APP_API_BASE_URL || 'http://localhost:8000/api';
 
 const app = createApp(App)
 app.use(router)
