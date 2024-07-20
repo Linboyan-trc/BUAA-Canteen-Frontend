e50fcc63f58c70cda125c69392d68fe2eccfaaa2
cubeSugar <wxbcly@163.com>
2024-07-20 13:04:57 +0800

    修改了一些配置

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # buaa-cafeterias
+`//temp`下面的代码都是测试数据，与后端对接时，注释掉测试数据代码，然后把附近原本注释掉的接口代码（一般在测试数据下方）取消注释
 
-This template should help get you started developing with Vue 3 in Vite.
+### 一些额外安装的库
+```sh
+npm install unplugin-auto-import
+npm install unplugin-vue-components
+npm install -D sass
+```
+不全，可以根据控制台运行的报错提示安装相应的库
 
 ## Recommended IDE Setup
 
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// element-plus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {ElementPlusResolver} from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    // element-plus
+    AutoImport({
+        resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+        // 1.配置elementPlus采用sass
+        resolvers: [ElementPlusResolver({importStyle: "sass"})],
+    }),
   ],
   resolve: {
     alias: {
@@ -21,5 +34,11 @@ export default defineConfig({
         rewrite: path => path.replace(/^\/api/, '') // 重写路径，将前缀 '/api' 移除
       }
     }
+  },
+  define: {
+    'process.env': {
+      VUE_APP_API_BASE_URL: JSON.stringify(process.env.VUE_APP_API_BASE_URL)
+    }
   }
 })
+
