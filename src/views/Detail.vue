b99230d547f449fa54799a9cd4965ebe9de86840
zz-orb <22373474@buaa.edu.cn>
2024-07-20 22:01:39 +0800

    详情页添加关闭按钮

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,16 +1,20 @@
 <script>
 import { onMounted } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import PostDetail from "@/components/PostDetail.vue";
 import { controlDetail } from "@/store/controlDetail";
 import { useUserStore } from "@/store/user";
+import { Close } from '@element-plus/icons-vue';
+
 export default {
   name: "Detail",
   components: {
     PostDetail,
+    Close,
   },
   setup() {
     const route = useRoute();
+    const router = useRouter();
     const userStore = useUserStore();
     const Detail = controlDetail();
     const detail = Detail.detail;
@@ -19,8 +23,12 @@ export default {
     const getDetail = () => Detail.getDetail(route.params.dishId); // 使用正确的路由参数名
     const afterDoComment = (comment) => Detail.afterDoComment(comment);
 
+    const closeDetail = () => {
+      router.back();
+    };
+
     onMounted(() => getDetail());
-    return { detail, afterDoComment, content };
+    return { detail, afterDoComment, content, closeDetail };
   }
 }
 </script>
@@ -28,6 +36,11 @@ export default {
 <template>
   <div class="box">
     <PostDetail :detail="detail" @afterDoComment="afterDoComment" v-if="detail.id" />
+    <div class="close-button" @click="closeDetail">
+      <el-icon>
+        <Close />
+      </el-icon>
+    </div>
   </div>
 </template>
 
@@ -39,5 +52,26 @@ export default {
   border-radius: 0.8rem;
   width: 1200px;
   height: 600px;
+}
+
+.close-button {
+  color: #4274b9;
+  position: absolute;
+  top: 20px;
+  left: 1190px;
+
+  cursor: pointer;
+  transition: transform 0.3s ease-in-out;
+  font-size: 2.5rem;
+}
+
+.close-button:hover {
+  transform: rotate(90deg);
+}
+
+.close-button > svg {
+  width: 25px;
+  height: 25px;
+  stroke-width: 2;
 }
 </style>
