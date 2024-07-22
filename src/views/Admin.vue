026cc614cd6fbd44641aa83be46401370b36ffe7
zz-orb <22373474@buaa.edu.cn>
2024-07-22 15:58:04 +0800

    管理员登录

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,10 +1,18 @@
 <template>
-
-</template>
-<script>
-export default {
+    <div>
+      <button @click="goToAdminLogin">Go to Admin Login</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
     name: "Admin",
-    setup() {
+    methods: {
+      goToAdminLogin() {
+        // 替换为你的Django管理页面的登录URL
+        this.$router.push("http://127.0.0.1:8000/admin/login/");
+      }
     }
-}
-</script>
+  }
+  </script>
+  
