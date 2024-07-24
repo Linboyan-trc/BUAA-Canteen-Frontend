1e848973a4a755bb83bf8bf3ab3be9fd0aeb666d
zz-orb <22373474@buaa.edu.cn>
2024-07-24 18:39:37 +0800

    引入@fortawesome/vue-fontawesome 更改吃过图标

--- src/components/PostDetail.vue
+++ src/components/PostDetail.vue
@@ -280,14 +280,12 @@ export default defineComponent({
             <div class="buttonArea">
               <el-row>
                 <el-button link class="warp" @click="doSomething('ate', detail)" :disabled="review">
-                  <svg x="1689147877558" class="icon" viewBox="0 0 1024 1024" version="1.1"
-                    xmlns="http://www.w3.org/2000/svg" p-id="3345" width="25" height="25">
-                    <path
-                      d="M512 901.746939c-13.583673 0-26.122449-4.179592-37.093878-13.061225-8.881633-7.314286-225.697959-175.020408-312.424489-311.379592C133.746939 532.37551 94.040816 471.24898 94.040816 384.522449c0-144.718367 108.146939-262.269388 240.326531-262.269388 67.395918 0 131.657143 30.82449 177.632653 84.636735 45.453061-54.334694 109.191837-84.636735 177.110204-84.636735 132.702041 0 240.326531 117.55102 240.326531 262.269388 0 85.159184-37.093878 143.673469-67.395919 191.216327l-1.044898 1.567346c-86.726531 136.359184-303.542857 304.587755-312.424489 311.379592-10.44898 8.359184-22.987755 13.061224-36.571429 13.061225z"
-                      :fill="!checkEat(detail.id) ? '#cecccc' : '#d81e06'" p-id="3346"></path>
-                  </svg>
-                  <el-text size="large" tag="b" type="info">{{ detail.ateCount }}</el-text>
-                </el-button>
+    <font-awesome-icon
+      :icon="['fas', 'utensils']"
+      :style="{ color: checkEat(detail.id) ? '#d81e06' : '#cecccc', width: '25px', height: '25px' }"
+    />
+    <el-text size="large" tag="b" type="info">{{ detail.ateCount }}</el-text>
+  </el-button>
                 <el-button link class="warp" @click="doSomething('collect', detail)" :disabled="review">
                   <svg x="1689148085763" class="icon" viewBox="0 0 1024 1024" version="1.1" style="margin-bottom: 4px;"
                     xmlns="http://www.w3.org/2000/svg" p-id="4912" width="25" height="25">
